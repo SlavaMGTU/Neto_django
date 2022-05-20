e6d15d099b033c02c83d99ebe2504ff61c191672
Viacheslav Grachev <slavamgtu@yandex.ru>
2022-05-20 22:54:16 +0400

    not load .json and work project

--- 2.1-databases/models_list_displaying/books/views.py
+++ 2.1-databases/models_list_displaying/books/views.py
@@ -1,7 +1,38 @@
 from django.shortcuts import render
 
+from books.models import Book
+
 
 def books_view(request):
     template = 'books/books_list.html'
-    context = {}
+
+    sort_by = request.GET.get('sort')
+    context = {'books':  Book.objects.all().order_by(sort_by)}
+    Book.objects().filter().orders_by().first()# Самая маленькая из больших
+    Book.objects().filter().orders_by().lost()# Самая маленькая из больших
     return render(request, template, context)
+
+# into file books_list.html
+# < div
+#
+#
+# class ="book col-md-4" >
+#
+# < h2 > Сияние < / h2 >
+# < p > Автор: Стивен
+# Кинг < / p >
+# < p > Дата
+# публикации: 2018 - 0
+# 9 - 10 < / p >
+# < / div >
+# < div
+#
+#
+# class ="book col-md-4" >
+#
+# < h2 > 1984 < / h2 >
+# < p > Автор: Джордж
+# Оруэл < / p >
+# < p > Дата
+# публикации: 2015 - 03 - 11 < / p >
+# < / div >
