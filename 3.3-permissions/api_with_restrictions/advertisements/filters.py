927b1d724a33a201159ec7efc2facce943b6688e
Viacheslav Grachev <slavamgtu@yandex.ru>
2022-08-02 21:42:41 +0400

    not All working and IsOwnerOrReadOnly isn't working too

--- 3.3-permissions/api_with_restrictions/advertisements/filters.py
+++ 3.3-permissions/api_with_restrictions/advertisements/filters.py
@@ -1,7 +1,7 @@
 from django_filters import rest_framework as filters
+from rest_framework import permissions
 
 from advertisements.models import Advertisement
-
 
 class AdvertisementFilter(filters.FilterSet):
     """Фильтры для объявлений."""
--- 3.3-permissions/api_with_restrictions/advertisements/views.py
+++ 3.3-permissions/api_with_restrictions/advertisements/views.py
@@ -5,23 +5,24 @@ from rest_framework.viewsets import ModelViewSet
 from advertisements.models import Advertisement
 from advertisements.serializers import AdvertisementSerializer
 
-
 class IsOwnerOrReadOnly(permissions.BasePermission):
-    """
-    Object-level permission to only allow owners of an object to edit it.
-    Assumes the model instance has an `owner` attribute.
-    """
-
+    # """
+    # Object-level permission to only allow owners of an object to edit it.
+    # Assumes the model instance has an `owner` attribute.
+    # """
+    #
+    # def has_object_permission(self, request, view, obj):
+    #     # Read permissions are allowed to any request,
+    #     # so we'll always allow GET, HEAD or OPTIONS requests.
+    #     if request.method in permissions.SAFE_METHODS:
+    #         return True
+    #
+    #     # Instance must have an attribute named `owner`.
+    #     return obj.owner == request.user
     def has_object_permission(self, request, view, obj):
-        # Read permissions are allowed to any request,
-        # so we'll always allow GET, HEAD or OPTIONS requests.
-        if request.method in permissions.SAFE_METHODS:
+        if request.method == 'GET':
             return True
-
-        # Instance must have an attribute named `owner`.
-        return obj.owner == request.user
-
-
+        return request.user.is_authenticated and obj.author == request.user
 
 
 
@@ -36,7 +37,7 @@ class AdvertisementViewSet(ModelViewSet):
     #throttle_classes = [AnonRateThrottle]
 
 
-    def get_permissions(self):
+    def get_permissions(self) -> object:
          """Получение прав для действий."""
          if self.action in ["create", "update", "partial_update"]:
              return [IsAuthenticated()]
