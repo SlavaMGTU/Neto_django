e609cb1a301146a9d4eb07012504a88c10446197
Вячеслав Грачев <47940826+SlavaMGTU@users.noreply.github.com>
2022-09-09 00:33:20 +0400

    Update permissions.py

--- 3.3-permissions/api_with_restrictions/advertisements/permissions.py
+++ 3.3-permissions/api_with_restrictions/advertisements/permissions.py
@@ -1,13 +1,5 @@
-from rest_framework import permissions
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import BasePermission
 
-from advertisements.models import Advertisement
-
-
-class IsOwnerOrReadOnly(permissions.BasePermission):
+class IsOwnerOrReadOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
-        if request.method == 'GET':
-            return True
-        if request.method == 'PATCH':
-            return obj.creator == request.user #request.user.is_authenticated and
-        return []
+        return request.user == obj.creator
