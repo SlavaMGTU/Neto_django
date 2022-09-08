e9c699d39f4f8ece580e03e65d8f9e3f22e187a0
Вячеслав Грачев <47940826+SlavaMGTU@users.noreply.github.com>
2022-09-09 00:34:29 +0400

    Update serializers.py

--- 3.3-permissions/api_with_restrictions/advertisements/serializers.py
+++ 3.3-permissions/api_with_restrictions/advertisements/serializers.py
@@ -1,9 +1,7 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
-
 from advertisements.models import Advertisement
-
 
 class UserSerializer(serializers.ModelSerializer):
     """Serializer для пользователя."""
@@ -11,7 +9,6 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'first_name', 'last_name',)
-
 
 class AdvertisementSerializer(serializers.ModelSerializer):
     """Serializer для объявления."""
@@ -22,17 +19,21 @@ class AdvertisementSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Advertisement
-        fields = ('id', 'title', 'description', 'creator',
-                  'status', 'created_at', )
+        fields = ('id', 'title', 'description', 'creator', 'status', 'created_at', )
 
     def create(self, validated_data):
         """Метод для создания"""
-
-        # Простановка значения поля создатель по-умолчанию. Текущий пользователь является создателем объявления
-        # изменить или переопределить его через API нельзя. обратите внимание на `context` – он выставляется автоматически
-        # через методы ViewSet.  само поле при этом объявляется как `read_only=True`
-        validated_data["creator"] = self.context['request'].user
-                # TODO: добавьте требуемую валидацию
-        if Advertisement.objects.filter(creator=validated_data['creator'], status='OPEN').count() >= 10:
-            raise ValidationError('Open Advertisments more then TEN')
+        validated_data["creator"] = self.context["request"].user
         return super().create(validated_data)
+
+    def validate(self, data):
+        """Метод для валидации. Вызывается при создании и обновлении."""
+        ad_user = self.context['request'].user
+        ad_quantity = Advertisement.objects.filter(status='OPEN', creator=ad_user)
+        if self.context['request'].method == 'POST' and len(ad_quantity) >= 10:
+            raise ValidationError('10 ad limit exceeded. you need to close unnecessary ads')
+        if self.context['request'].method == 'PATCH' and data.get('status') == 'OPEN' and len(ad_quantity) >= 10:
+            raise ValidationError('10 ad limit exceeded. you need to close unnecessary ads')
+        if self.context['request'].method == 'PUT' and data.get('status') == 'OPEN' and len(ad_quantity) >= 10:
+            raise ValidationError('10 ad limit exceeded. you need to close unnecessary ads')
+        return data
