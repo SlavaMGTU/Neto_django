23f3d88e990d187167accb40128c5f760dd332b2
Вячеслав Грачев <47940826+SlavaMGTU@users.noreply.github.com>
2022-09-09 00:34:58 +0400

    Update views.py

--- 3.3-permissions/api_with_restrictions/advertisements/views.py
+++ 3.3-permissions/api_with_restrictions/advertisements/views.py
@@ -1,8 +1,7 @@
-from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.throttling import AnonRateThrottle
 from rest_framework.viewsets import ModelViewSet
 
+from advertisements.filters import AdvertisementFilter
 from advertisements.models import Advertisement
 from advertisements.permissions import IsOwnerOrReadOnly
 from advertisements.serializers import AdvertisementSerializer
@@ -10,19 +9,18 @@ from advertisements.serializers import AdvertisementSerializer
 
 class AdvertisementViewSet(ModelViewSet):
     """ViewSet для объявлений."""
-
-    # TODO: настройте ViewSet, укажите атрибуты для кверисета,
-    #   сериализаторов и фильтров
     queryset = Advertisement.objects.all()
     serializer_class = AdvertisementSerializer
-    permission_classes = [IsOwnerOrReadOnly]
-    throttle_classes = [AnonRateThrottle]
-    filter_backends = [DjangoFilterBackend,]
-    filterset_fields = ['creator', 'created_at',]
-    def get_permissions(self) -> object:
-         """Получение прав для действий."""
-         if self.action in ["create", "update", "partial_update"]:
-             return [IsAuthenticated()]
-         return []
+    print('Check IT!!!!2')
+    filterset_fields = ['created_at', 'status', 'creator']
+    filterset_class = AdvertisementFilter
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+
+    def get_permissions(self):
+        """Получение прав для действий."""
+        if self.action in ["create", "update", "partial_update", "destroy"]:
+            return [IsAuthenticated(), IsOwnerOrReadOnly()]
+        else:
+            return []
 
 
